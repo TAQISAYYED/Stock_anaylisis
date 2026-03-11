a23f62b1277842ceb1a43e7be150f1f42d03fbf8
SAYYEDTAQI <sayyedmohdtaqi4@gmail.com>
2026-03-11 22:26:41 +0530

    Frontend changes

--- frontend/src/components/PortfolioChart.jsx
+++ frontend/src/components/PortfolioChart.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import api from "../services/api";
-import "./portfoliochart.css";
+import "./PortfolioChart.css";
 
 export default function PortfolioChart() {
   const [portfolios, setPortfolios] = useState([]);
